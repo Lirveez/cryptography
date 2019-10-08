0b9cd8dcfe45a55a45d6524aafd69b0596bf0a3a
Nikita Medvedev <nikita.medvedev@leroymerlin.ru>
2019-10-08 19:05:18 +0300

    Using Fermat number

--- src/main/java/ru/lirveez/cryptography/H/KeyGenerator.java
+++ src/main/java/ru/lirveez/cryptography/H/KeyGenerator.java
@@ -9,6 +9,12 @@ import java.util.Random;
 @NoArgsConstructor
 public class KeyGenerator {
 
+    private static final BigInteger[] FERMAT_NUMBER = new BigInteger[]{
+            BigInteger.valueOf(17),
+            BigInteger.valueOf(257),
+            BigInteger.valueOf(65537L)
+    };
+
     public Key generateKey(int bitLength) {
         val p = BigInteger.probablePrime(bitLength, new Random());
         val q = BigInteger.probablePrime(bitLength, new Random());
@@ -18,9 +24,8 @@ public class KeyGenerator {
     public Key generateFor(BigInteger q, BigInteger p) {
         val n = p.multiply(q);
         val eulerFunction = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-        val openExponent = BigInteger.valueOf(65537L); // TODO: 07.10.2019 Generate
+        val openExponent = FERMAT_NUMBER[new Random().nextInt(FERMAT_NUMBER.length)];
         val d = openExponent.modInverse(eulerFunction);
         return new Key(openExponent, n, d);
     }
-
 }
--- src/main/java/ru/lirveez/cryptography/H/Launcher.java
+++ src/main/java/ru/lirveez/cryptography/H/Launcher.java
@@ -3,11 +3,42 @@ package ru.lirveez.cryptography.H;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
 @Slf4j
 public class Launcher {
 
+    public static final String TEXT = "Какой-то секретный текст";
+
     public static void main(String[] args) {
-        val key = new KeyGenerator().generateKey(10);
+        val key = new KeyGenerator().generateKey(1024);
         log.info("Key is {}", key);
+        byte[] bytedText = TEXT.getBytes(Charset.defaultCharset());
+        log.info("TEXT: {}", bytedText);
+
+
+        val binaryText = rawText(TEXT);
+        val rsaEncoder = new RsaEncoder(key);
+        val encoded = rsaEncoder.encode(binaryText);
+        log.info("Byted text: {}", binaryText.toString(2));
+        log.info("Encoded: {}", encoded.toString(2));
+        val decoded = rsaEncoder.decode(encoded);
+        log.info("Decoded: {}", decoded.toString(2));
+        val str = fromBigInt(decoded);
+        log.info("String is {}", str);
+    }
+
+    private static BigInteger rawText(String text) {
+        val bytes = text.getBytes(Charset.defaultCharset());
+        var bi = new BigInteger("0");
+        for (val b : bytes) {
+            val temp = BigInteger.valueOf(Byte.toUnsignedLong(b));
+            bi = bi.shiftLeft(8).or(temp);
+        }
+        return bi;
+    }
+    private static String fromBigInt(BigInteger bi) {
+        return new String(bi.toByteArray(), Charset.defaultCharset());
     }
 }
